71aebba55b364889ba47bdc46b437598c1167058
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-05 12:13:55 +0200

    Main controller have new init method

--- Controller/MainController.py
+++ Controller/MainController.py
@@ -1,5 +1,12 @@
+# import Model
+import View
+import Controller
+
 
 class MainController:
+    def __init__(self):
+        self.chessTournamentController = Controller.ChessTournamentController
+        self.mainView = View.MainView
 
-
-    __init__()
+    def start(self):
+        self.mainView.display_main_menu()
