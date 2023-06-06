3215661a602431305dc832d08c2e20e90402a7ac
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-06 17:15:28 +0200

    The retrieval of the user's decision is now the responsibility of the MainController.

--- View/MainView.py
+++ View/MainView.py
@@ -2,6 +2,14 @@ class MainView:
 
     @staticmethod
     def display_main_menu():
-        print("Welcome to the chess center. Please choose one of the following options:")
-        print("1 - Create new tournament.")
-        print("2 - Display reports")
+        print("""Welcome to the chess center. Please choose one the following options"
+        1 - Create new tournament
+        2 - Display reports""")
+
+    @staticmethod
+    def display_options():
+        print("Please, make your selection:\n")
+
+    @staticmethod
+    def display_message_error():
+        print("Invalid answer, please try again")
