8ebd3d26c080f531ea9c319270a9c933ce2eed76
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-09 06:28:43 +0200

    The view is now responsible for retrieving the decision made by the user.

--- View/MainView.py
+++ View/MainView.py
@@ -1,15 +1,26 @@
 class MainView:
+    NUMBER_OF_OPTIONS: int = 2
 
-    @staticmethod
-    def display_main_menu():
+    def display_main_menu(self):
         print("""Welcome to the chess center. Please choose one the following options"
         1 - Create new tournament
         2 - Display reports""")
 
-    @staticmethod
-    def display_options():
+    def display_options(self):
         print("Please, make your selection:\n")
 
-    @staticmethod
-    def display_message_error():
+    def display_message_error(self):
         print("Invalid answer, please try again")
+
+    def get_choice(self) -> int:
+        numero = -1
+        self.display_options()
+        while numero > self.NUMBER_OF_OPTIONS or numero <= 0:
+            try:
+                numero = int(input())
+                if numero > self.NUMBER_OF_OPTIONS or numero <= 0:
+                    self.display_message_error()
+            except ValueError:
+                self.display_message_error()
+                numero = -1
+        return numero
