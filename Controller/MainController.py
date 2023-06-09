864c5d72f3fe98bd4e1271398d06fde6ab4579f2
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-09 06:27:54 +0200

    The implementation of the method to retrieve the user's decision has been transferred to the view.

--- Controller/MainController.py
+++ Controller/MainController.py
@@ -3,31 +3,16 @@ import Controller
 
 
 class MainController:
-    NUMBER_OF_OPTIONS: int = 2
 
     def __init__(self):
-        self.chessTournamentController = Controller.ChessTournamentController
+        self.chess_tournament_controller = Controller.ChessTournamentController()
         self.main_view = View.MainView()
 
     def start(self):
         self.main_view.display_main_menu()
-        choice = self.get_choice()
+        choice = self.main_view.get_choice()
         match choice:
             case 1:
-                chess_tournament_controller = Controller.ChessTournamentController()
-                chess_tournament_controller.start_tournament()
+                self.chess_tournament_controller.start_tournament()
             case 2:
                 print("Under construction")
-
-    def get_choice(self) -> int:
-        numero = -1
-        self.main_view.display_options()
-        while numero > self.NUMBER_OF_OPTIONS or numero <= 0:
-            try:
-                numero = int(input())
-                if numero > self.NUMBER_OF_OPTIONS or numero <= 0:
-                    self.main_view.display_message_error()
-            except ValueError:
-                self.main_view.display_message_error()
-                numero = -1
-        return numero
