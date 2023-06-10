e8a53c431027d2c5cc2752b307d74640600d6863
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-10 17:29:04 +0200

    Now the score attribute can be added to the player when initializing an instance.

--- Model/Player.py
+++ Model/Player.py
@@ -1,3 +1,5 @@
+import Model
+
 
 class Player:
     score: float = 0
@@ -5,11 +7,13 @@ class Player:
     def __init__(self, first_name: str,
                  last_name: str,
                  date_of_birth: str,
-                 chess_national_id: str):
+                 chess_national_id: str,
+                 score: float = 0):
         self.first_name = first_name.lower()
         self.last_name = last_name.upper()
         self.date_of_birth = date_of_birth
         self.chess_national_id = chess_national_id
+        self.score = score
 
     def update_score(self, score: float):
         self.score += score
