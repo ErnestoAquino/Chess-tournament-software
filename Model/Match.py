e46d2feb1569edd680e9b556c800d6ca108358ff
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-11 11:26:19 +0200

    The  class now takes care of determining which player will play with the white pieces and which player will play with the black pieces.

--- Model/Match.py
+++ Model/Match.py
@@ -16,6 +16,8 @@ class Match:
     def __init__(self, player1: Player, player2: Player):
         self.player1 = player1
         self.player2 = player2
+        self.white_player = random.choice([player1, player2])
+        self.black_player = player1 if self.white_player == player2 else player2
 
     def set_result_test(self, result: Result):
         match result:
@@ -28,4 +30,21 @@ class Match:
             case Result.DRAW:
                 self.player1.update_score(result.value)
                 self.player2.update_score(result.value)
+                self.result = result
+
+    def print_color_players(self):
+        print(f"{self.white_player.first_name} play with whites pieces")
+        print(f"{self.black_player.first_name} play with black pieces")
+
+    def set_result(self, result: Result):
+        match result:
+            case Result.WIN:
+                self.white_player.update_score(result.value)
+                self.result = result
+            case Result.LOSS:
+                self.black_player.update_score(1)
+                self.result = result
+            case Result.DRAW:
+                self.white_player.update_score(result.value)
+                self.black_player.update_score(result.value)
                 self.result = result
