5e18160726060a2175e9ae0ba4157103b5ce692b
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-12 06:27:26 +0200

    AddPlayerController has been added.

--- Controller/__init__.py
+++ Controller/__init__.py
@@ -1,2 +1,3 @@
 from Controller.ChessTournamentController import ChessTournamentController
 from Controller.MainController import MainController
+from Controller.AddPlayerController import AddPlayerController
