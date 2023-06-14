afd455dd2af25f087507fcac97f0e791496fff7e
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-14 11:15:49 +0200

    The self reference has been added to the player1 and player2 classes.

--- Model/Match.py
+++ Model/Match.py
@@ -16,21 +16,8 @@ class Match:
     def __init__(self, player1: Player, player2: Player):
         self.player1 = player1
         self.player2 = player2
-        self.white_player = random.choice([player1, player2])
+        self.white_player = random.choice([self.player1, self.player2])
         self.black_player = player1 if self.white_player == player2 else player2
-
-    def set_result_test(self, result: Result):
-        match result:
-            case Result.WIN:
-                self.player1.update_score(result.value)
-                self.result = result
-            case Result.LOSS:
-                self.player2.update_score(1)
-                self.result = result
-            case Result.DRAW:
-                self.player1.update_score(result.value)
-                self.player2.update_score(result.value)
-                self.result = result
 
     def print_color_players(self):
         print(f"{self.white_player.first_name} play with whites pieces")
