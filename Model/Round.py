40991b7919208623c970d9e76b16d77febc56817
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-19 22:12:00 +0200

    A method has been created that generates the matches.

--- Model/Round.py
+++ Model/Round.py
@@ -1,3 +1,7 @@
+import random
+from typing import List
+from typing import Set
+from typing import Tuple
 import datetime
 import Model
 
@@ -8,6 +12,7 @@ class Round:
 
     def __init__(self, name: str):
         self.name = name
+        self.list_of_matches = []
         self.start_datetime = str(datetime.datetime.now())
 
     def add_mach(self, match: Model.Match):
@@ -15,3 +20,18 @@ class Round:
 
     def write_end_datetime(self):
         self.end_datetime = str(datetime.datetime.now())
+
+    def generate_matches(self, players: List[Model.Player], previous_matches: Set[Tuple[Model.Player, Model.Player]]) \
+            -> List[Tuple[Model.Player, Model.Player]]:
+        emparejamientos = []
+        jugadores_disponibles = players[:]
+        random.shuffle(jugadores_disponibles)
+
+        for i in range(0, len(jugadores_disponibles), 2):
+            player1 = jugadores_disponibles[i]
+            player2 = jugadores_disponibles[i + 1]
+            if (player1, player2) not in previous_matches and (player2, player1) not in previous_matches:
+                emparejamientos.append((player1, player2))
+                previous_matches.add((player1, player2))
+        return emparejamientos
+
