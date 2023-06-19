f0968ab84e418c0af676be911593cfe817e08d92
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-19 22:11:02 +0200

    A list has been created to store the players they have faced.

--- Model/Player.py
+++ Model/Player.py
@@ -3,6 +3,8 @@ import Model
 
 class Player:
     score: float = 0
+    played_players = []
+    id_played = []
 
     def __init__(self, first_name: str,
                  last_name: str,
@@ -14,6 +16,17 @@ class Player:
         self.date_of_birth = date_of_birth
         self.chess_national_id = chess_national_id
         self.score = score
+        self.played_players = []
+        self.id_played = []
 
     def update_score(self, score: float):
         self.score += score
+
+    def test_print_opponents(self):
+        if len(self.played_players) != 0:
+            for player in self.played_players:
+                print(f"Ya he  combatido a: {player.first_name}")
+
+    def test_print_id_opponents(self):
+        for id in self.id_played:
+            print(f"{id}")
