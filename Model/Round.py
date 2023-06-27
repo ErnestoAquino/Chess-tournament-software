ce45d9d00c1acdfcb2c3ffa1e3bf88295a647f93
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-27 15:35:40 +0200

    The method that generated the matches has been removed, now it is the controller's responsibility.

--- Model/Round.py
+++ Model/Round.py
@@ -1,7 +1,3 @@
-import random
-from typing import List
-from typing import Set
-from typing import Tuple
 import datetime
 import Model
 
@@ -20,18 +16,3 @@ class Round:
 
     def write_end_datetime(self):
         self.end_datetime = str(datetime.datetime.now())
-
-    def generate_matches(self, players: List[Model.Player], previous_matches: Set[Tuple[Model.Player, Model.Player]]) \
-            -> List[Tuple[Model.Player, Model.Player]]:
-        emparejamientos = []
-        jugadores_disponibles = players[:]
-        random.shuffle(jugadores_disponibles)
-
-        for i in range(0, len(jugadores_disponibles), 2):
-            player1 = jugadores_disponibles[i]
-            player2 = jugadores_disponibles[i + 1]
-            if (player1, player2) not in previous_matches and (player2, player1) not in previous_matches:
-                emparejamientos.append((player1, player2))
-                previous_matches.add((player1, player2))
-        return emparejamientos
-
