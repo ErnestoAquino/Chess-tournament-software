a6b2b3b497d2a6c2372a4a81dadcac1acb208067
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-06-27 15:33:28 +0200

    The present_round method now receives a second parameter to display the round number

--- Controller/ChessTournamentController.py
+++ Controller/ChessTournamentController.py
@@ -1,3 +1,7 @@
+from typing import List
+from typing import Set
+from typing import Tuple
+import itertools
 import Model
 import View
 
@@ -5,6 +9,7 @@ import View
 class ChessTournamentController:
     NUMBER_CHESS_PLAYERS: int
     response: str = None
+    # previous_matches = set()
 
     def __init__(self, number_chess_players: int = 4):
         self.tournament = None
@@ -12,60 +17,129 @@ class ChessTournamentController:
         self.chess_tournament_view = View.ChessTournamentView()
 
     def create_tournament(self, name: str, location: str):
-        self.tournament = Model.Tournament(name=name, location=location)
+        self.tournament = Model.Tournament(name = name, location = location)
 
     def register_player(self,
                         first_name: str,
                         last_name: str,
                         date_of_birth: str,
                         chess_national_id: str):
-        player = Model.Player(first_name=first_name,
-                              last_name=last_name,
-                              date_of_birth=date_of_birth,
-                              chess_national_id=chess_national_id)
+        player = Model.Player(first_name = first_name,
+                              last_name = last_name,
+                              date_of_birth = date_of_birth,
+                              chess_national_id = chess_national_id)
         self.tournament.add_player(player)
 
     def start_tournament(self):
         self.chess_tournament_view.display_message_tournament_creation()
-        tournament_name = self.chess_tournament_view.get_user_response(information_request="Tournament Name: ")
-        tournament_location = self.chess_tournament_view.get_user_response(information_request="Tournament Location: ")
-        self.create_tournament(name=tournament_name, location=tournament_location)
-        tournament_description = self.chess_tournament_view.get_user_response(information_request="Tournament "
-                                                                                                  "Description: ")
+        tournament_name = self.chess_tournament_view.get_user_response(information_request = "Tournament Name: ")
+        tournament_location = self.chess_tournament_view.get_user_response(
+            information_request = "Tournament Location: ")
+        self.create_tournament(tournament_name, tournament_location)
+        tournament_description = self.chess_tournament_view.get_user_response(information_request = "Tournament "
+                                                                                                    "Description: ")
         self.tournament.write_description(tournament_description)
-        self.chess_tournament_view.display_message_success(tournament_name)
+        self.chess_tournament_view.display_tournament_information(self.tournament)
         self.tournament.load_players()
-        # self.chess_tournament_view.present_players(self.tournament.players)
         self.chess_tournament_view.display_scores(self.tournament.players)
-        self.tournament.start()
-        self.present_rounds()
+        self.create_the_rounds()
+        self.tournament.sort_players()
         self.chess_tournament_view.display_scores(self.tournament.players)
-        # self.chess_tournament_view.display_tournament_information(self.tournament)
-        # self.register_players(number_of_players=self.NUMBER_CHESS_PLAYERS)
-        # self.chess_tournament_view.present_players(players=self.tournament.players)
-        # self.tournament.save_players()
 
     def register_players(self, number_of_players: int):
         self.chess_tournament_view.display_message_register_player()
         for i in range(number_of_players):
             print(f"Player number {i + 1} registration:")
-            first_name = self.chess_tournament_view.get_user_response(information_request="First Name: ")
-            last_name = self.chess_tournament_view.get_user_response(information_request="Last Name: ")
-            date_of_birth = self.chess_tournament_view.get_user_response(information_request="Date of Birth: ")
-            national_id = self.chess_tournament_view.get_user_response(information_request="Chess National ID: ")
+            first_name = self.chess_tournament_view.get_user_response(information_request = "First Name: ")
+            last_name = self.chess_tournament_view.get_user_response(information_request = "Last Name: ")
+            date_of_birth = self.chess_tournament_view.get_user_response(information_request = "Date of Birth: ")
+            national_id = self.chess_tournament_view.get_user_response(information_request = "Chess National ID: ")
             player = Model.Player(first_name,
                                   last_name,
                                   date_of_birth,
                                   national_id)
             self.tournament.add_player(player)
 
-    def present_rounds(self):
-        for round in self.tournament.rounds:
-            print(f"Round {round.name}")
-            print(f"Star Date: {round.start_datetime}")
-            for match in round.list_of_matches:
-                print(f"Match {match.player1.first_name} VS {match.player2.first_name}")
-                match.print_color_players()
-                print("---------------")
-                result = self.chess_tournament_view.get_result_of_match()
-                match.set_result(result)
+    def present_round(self, round_to_show: Model.Round):
+        self.chess_tournament_view.display_round(round_to_show)
+        for i, match in enumerate(round_to_show.list_of_matches):
+            self.chess_tournament_view.display_match(match, number_match = i)
+            result = self.chess_tournament_view.get_result_of_match()
+            match.set_result(result)
+        round_to_show.write_end_datetime()
+
+    def create_first_round(self):
+        self.tournament.shuffle_players()
+        first_round = Model.Round(name = "Round 1")
+        for i in range(0, len(self.tournament.players), 2):
+            player1 = self.tournament.players[i]
+            player2 = self.tournament.players[i + 1]
+            player1.played_players.append(player2)
+            player2.played_players.append(player1)
+            match = Model.Match(player1, player2)
+            first_round.add_mach(match)
+        self.tournament.rounds.append(first_round)
+        self.tournament.current_round += 1
+
+    def generate_matches(self, previous_matches: Set[frozenset]) \
+            -> List[Model.Match]:
+        self.tournament.sort_players()
+        pairings = []
+        available_players = self.tournament.players[:]
+        for i in range(0, len(available_players), 2):
+            player1 = available_players[i]
+            player2 = available_players[i + 1]
+            if not any({player1, player2} == match or {player2, player1} == match for match in previous_matches):
+                pairings.append(Model.Match(player1, player2))
+                previous_matches.add(frozenset({player1, player2}))
+        return pairings
+
+    def generate_matches_test(self) -> List[Model.Match]:
+        self.tournament.sort_players()
+        pairings = []
+        paired_players = []
+        players_with_pair = []
+
+        for i, player in enumerate(self.tournament.players):
+            for j, pair in enumerate(itertools.combinations(self.tournament.players, 2)):
+                if pair[1] not in pair[0].played_players and pair[0] not in pair[1].played_players and \
+                    player == pair[0] and pair not in paired_players and pair[0] not in players_with_pair \
+                        and pair[1] not in players_with_pair:
+                    paired_players.append(pair)
+                    player1 = pair[0]
+                    player2 = pair[1]
+                    # pairings.append(pair)
+                    pairings.append(Model.Match(player1, player2))
+                    players_with_pair.append(pair[0])
+                    players_with_pair.append(pair[1])
+
+        possibles_chess_games = itertools.combinations(self.tournament.players, 2)
+        for possible_game in possibles_chess_games:
+            player1, player2 = possible_game
+            print(player1.first_name, player2.first_name)
+        return pairings
+
+    def create_round(self, number_of_round: int):
+        self.tournament.sort_players()
+        new_round = Model.Round(name = f"Round {number_of_round}")
+        pairings = self.generate_matches_test()
+        print(len(pairings), "EMPAREJAMIENTOS")
+        for match in pairings:
+            match.player1.played_players.append(match.player2)
+            match.player2.played_players.append(match.player1)
+            new_round.add_mach(match)
+        self.tournament.rounds.append(new_round)
+
+    def create_the_rounds(self):
+        self.create_first_round()
+        self.present_round(round_to_show = self.tournament.rounds[0])
+        for round_number in range(2, self.tournament.number_of_rounds + 1):
+            self.create_round(round_number)
+            self.present_round(self.tournament.rounds[round_number - 1])
+            print(len(self.tournament.rounds[round_number - 1].list_of_matches))
+            self.print_played_player()
+
+    def print_played_player(self):
+        for player in self.tournament.players:
+            print(f"{player.first_name}")
+            player.test_print_opponents()
