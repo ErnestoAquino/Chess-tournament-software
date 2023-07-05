990f802e3ce77531a50988be95afba0a0cea1bc0
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-07-05 12:44:58 +0200

    The ReportController class has been added to the file.

--- Controller/__init__.py
+++ Controller/__init__.py
@@ -1,3 +1,4 @@
 from Controller.ChessTournamentController import ChessTournamentController
 from Controller.MainController import MainController
 from Controller.AddPlayerController import AddPlayerController
+from Controller.ReportsController import ReportsController
