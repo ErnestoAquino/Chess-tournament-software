3d5c2dcc0e2581f55d16c953ecf89ec3de3d5a3a
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-07-10 09:57:26 +0200

    A step has been added to check if an unfinished tournament exists in the database.

--- Controller/MainController.py
+++ Controller/MainController.py
@@ -1,5 +1,6 @@
 import View
 import Controller
+import DataBaseManager
 
 
 class MainController:
@@ -9,14 +10,19 @@ class MainController:
         self.add_player_controller = Controller.AddPlayerController()
         self.reports_controller = Controller.ReportsController()
         self.main_view = View.MainView()
+        self.database_manager = DataBaseManager.DataBaseManager()
 
     def start(self):
-        self.main_view.display_main_menu()
-        choice = self.main_view.get_choice()
-        match choice:
-            case 1:
-                self.chess_tournament_controller.start_tournament()
-            case 2:
-                self.reports_controller.start()
-            case 3:
-                self.add_player_controller.get_players_to_save()
+        if self.database_manager.check_unfinished_tournament() and self.main_view.get_yes_or_no_choice() == "yes":
+            # Todo Write code to load unfinished tournament.
+            print("Here is the code to resume the unfinished tournament")
+        else:
+            self.main_view.display_main_menu()
+            choice = self.main_view.get_choice()
+            match choice:
+                case 1:
+                    self.chess_tournament_controller.start_tournament()
+                case 2:
+                    self.reports_controller.start()
+                case 3:
+                    self.add_player_controller.get_players_to_save()
