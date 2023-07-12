cb9ec66aaf45093f8e66580781cb507d53b9e10a
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-07-12 17:57:19 +0200

    The  method has been started to be used when the user decides to continue the interrupted tournament.

--- Controller/MainController.py
+++ Controller/MainController.py
@@ -15,6 +15,7 @@ class MainController:
     def start(self):
         if self.database_manager.check_unfinished_tournament() and self.main_view.get_yes_or_no_choice() == "yes":
             # Todo Write code to load unfinished tournament.
+            self.database_manager.load_unfinished_tournament()
             print("Here is the code to resume the unfinished tournament")
         else:
             self.database_manager.delete_unfinished_tournament()
