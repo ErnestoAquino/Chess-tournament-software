a036d0b5a98e98beaa6ab21c465b606cd783f5e4
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-07-18 23:19:48 +0200

    The documentation for the methods of the class has been added.

--- Model/Round.py
+++ Model/Round.py
@@ -1,33 +1,74 @@
 from typing import Dict
 from typing import Any
+from typing import List
 import datetime
 import Model
 
 
 class Round:
-    list_of_matches = []
+    """
+      Represents a round in a tournament.
+
+      Attributes:
+          list_of_matches (List[Model.Match]): List of matches in the round.
+          name (str): Name of the round.
+          start_datetime (str): Start datetime of the round.
+          end_datetime (str): End datetime of the round.
+          finished (bool): Indicates if the round has finished.
+      """
+    list_of_matches: List[Model.Match] = []
 
     def __init__(self, name: str):
+        """
+        Initializes a Round instance.
+
+        Args:
+            name (str): Name of the round.
+        """
         self.name = name
         self.list_of_matches = []
         self.start_datetime = str(datetime.datetime.now())
         self.end_datetime = None
         self.finished = False
 
-    def add_mach(self, match: Model.Match):
+    def add_mach(self, match: Model.Match) -> None:
+        """
+        Adds a match to the round.
+
+        Args:
+            match (Match): Match to be added.
+        """
         self.list_of_matches.append(match)
 
-    def write_end_datetime(self):
+    def write_end_datetime(self) -> None:
+        """
+        Writes the end datetime of the round.
+        """
         self.end_datetime = str(datetime.datetime.now())
         self.set_finished()
 
     def is_finished(self) -> bool:
+        """
+        Checks if the round has finished.
+
+        Returns:
+            bool: True if the round has finished, False otherwise.
+        """
         return self.finished
 
-    def set_finished(self):
+    def set_finished(self) -> None:
+        """
+        Sets the round as finished.
+        """
         self.finished = True
 
-    def to_dictionary(self) -> Dict:
+    def to_dictionary(self) -> Dict[str, Any]:
+        """
+        Converts the round object to a dictionary.
+
+        Returns:
+            dict: Dictionary representation of the round.
+        """
         dictionary = {
             "name": self.name,
             "start_datetime": self.start_datetime,
@@ -40,7 +81,16 @@ class Round:
 
     @classmethod
     def load_from_dictionary(cls, data: Dict[str, Any]) -> 'Round':
-        round = Model.Round(data["name"])
+        """
+        Creates a round object from a dictionary.
+
+        Args:
+            data (dict): Dictionary containing the round data.
+
+        Returns:
+            Round: Round object created from the dictionary.
+        """
+        round = cls(data["name"])
         round.start_datetime = data["start_datetime"]
         if data["finished"] == "True":
             round.set_finished()
