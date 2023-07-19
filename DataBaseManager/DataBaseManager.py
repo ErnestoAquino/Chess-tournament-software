7a82adebfee31007622c1323a530776e513fed7a
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-07-19 14:15:06 +0200

    The documentation for the methods of the class has been added.

--- DataBaseManager/DataBaseManager.py
+++ DataBaseManager/DataBaseManager.py
@@ -1,7 +1,7 @@
 from tinydb import TinyDB
-from tinydb import Query
-from typing import Union
-from typing import List
+from typing import Any
+from typing import Optional
+from typing import Dict
 import Model
 
 
@@ -9,14 +9,31 @@ class DataBaseManager:
     tournament_temporary_id = None
 
     def __init__(self):
+        """
+        Initializes a new instance of the DataBaseManager class.
+
+        Creates connections to the player, tournament and cache databases.
+        """
         self.data_base_players = TinyDB("Data/Players/Players.json")
         self.data_base_tournaments = TinyDB("Data/Tournaments/Tournaments.json")
-        self.data_base_temporary_tournament = TinyDB("Data/Tournaments/Temporary_Tournament.json")
+        self.data_base_cache = TinyDB("Data/Tournaments/Cache.json")
 
     def load_all_players(self) -> [Model.Player]:
+        """
+        Loads all players from the player database.
+
+        Returns:
+            List[Model.Player]: A list of all the players.
+        """
         recovered_players = []
         local_players = self.data_base_players.table("Players")
-        results = local_players.all()
+        try:
+            results = local_players.all()
+        except Exception as e:
+            # Handle the case where the table does not exist or is empty
+            print(f"Error: {e}")
+            return recovered_players
+
         for result in results:
             player = Model.Player(first_name=result["first_name"],
                                   last_name=result["last_name"],
@@ -27,6 +44,12 @@ class DataBaseManager:
         return recovered_players
 
     def save_tournament(self, tournament_to_save: Model.Tournament):
+        """
+        Saves the tournament to the tournament database.
+
+        Args:
+            tournament_to_save (Model.Tournament): The tournament object to be saved.
+        """
         data_base = self.data_base_tournaments
         tournaments_table = data_base.table("tournaments")
         rounds_table = data_base.table("rounds")
@@ -71,169 +94,54 @@ class DataBaseManager:
             players_table.insert(player_data)
         self.delete_unfinished_tournament()
 
-    def checkpoint_creation_tournament(self, tournament: Model.Tournament):
-        data_base = self.data_base_temporary_tournament
-        tournaments_table = data_base.table("tournaments")
-        tournament_data = {
-            "name": tournament.name,
-            "location": tournament.location,
-            "start_date": tournament.start_date
-        }
-        self.tournament_temporary_id = tournaments_table.insert(tournament_data)
-
-    def checkpoint_add_tournament_description(self, description: str):
-        tournament_found = \
-            self.data_base_temporary_tournament.table("tournaments").get(doc_id=self.tournament_temporary_id)
-        if tournament_found:
-            tournament_found["description"] = description
-            self.data_base_temporary_tournament.table("tournaments").update(tournament_found)
-
     def check_unfinished_tournament(self) -> bool:
-        tournament_table = self.data_base_temporary_tournament.table("tournaments")
+        """
+        Checks if there is an unfinished tournament in the cache.
+
+        Returns:
+            bool: True if there is an unfinished tournament, False otherwise.
+        """
+        tournament_table = self.data_base_cache.table("tournaments")
         tournament_result = tournament_table.all()
         if len(tournament_result) == 0:
             return False
         else:
             return True
 
-    def update_result_match(self, round_id: int, number_of_match: int, result: Model.Result):
-        match_table = self.data_base_temporary_tournament.table("matches")
-        matches = match_table.search(Query().round_id == round_id)
-        print("================================================================================")
-        print(f"round id = {round_id}")
-        print(f"number of match = {number_of_match + 1}")
-        print(f"result {result.name}")
-        if number_of_match < len(matches):
-            desired_match = matches[number_of_match]
-            print(f"antes de actualizar: {desired_match['result']}")
-            desired_match["result"] = result.name
-            match_table.update(desired_match, doc_ids=[desired_match.doc_id])
-            print(matches[number_of_match]["white_player_first_name"])
-            print(f"despues: {matches[number_of_match]['result']}")
+    def make_check_point_tournament(self, data: [str, Any]) -> None:
+        """
+        Makes a checkpoint of the tournament by saving the data to the cache.
+
+        Args:
+            data ([str, Any]): The tournament data to be saved.
+        """
+        data_base = self.data_base_cache
+        tournaments_table = data_base.table("tournaments")
+        try:
+            tournaments_table.truncate()
+        except AttributeError:
+            print("Error: The tournaments table is empty.")
+        self.tournament_temporary_id = tournaments_table.insert(data)
+
+    def load_unfinished_tournament(self) -> Optional[Dict[str, Any]]:
+        """
+        Loads the unfinished tournament data from the cache.
+
+        Returns:
+            Optional[Dict[str, Any]]: The tournament data if available, None otherwise.
+        """
+        tournament_table = self.data_base_cache.table("tournaments")
+        tournament_list = tournament_table.all()
+        if tournament_list:
+            tournament_data = tournament_list[0]
+            self.tournament_temporary_id = tournament_data.doc_id
+            return tournament_data
         else:
-            print("No encontre el partido")
-        print("================================================================================")
-
-    def update_end_datetime_round(self, round_id: int, datetime: str):
-        rounds_table = self.data_base_temporary_tournament.table("rounds")
-        desired_round = rounds_table.get(doc_id=round_id)
-        if desired_round:
-            print("================================================================================")
-            desired_round["end_datetime"] = datetime
-            rounds_table.update(desired_round)
-            print(desired_round["end_datetime"])
-            print("================================================================================")
-        else:
-            print("No he encontrado el round")
-
-    def update_score_player(self, id_player: str):
-        pass
-
-    def checkpoint_match(self, match: Model.Match, round_id: int):
-        match_table = self.data_base_temporary_tournament.table("matches")
-        match_data = {
-            "white_player_first_name": match.white_player.first_name,
-            "white_player_last_name": match.white_player.last_name,
-            "white_player_date_of_birth": match.white_player.date_of_birth,
-            "white_player_chess_national_id": match.white_player.chess_national_id,
-            "white_player_score": match.white_player.score,
-            "black_player_first_name": match.black_player.first_name,
-            "black_player_last_name": match.black_player.last_name,
-            "black_player_date_of_birth": match.black_player.date_of_birth,
-            "black_player_chess_national_id": match.black_player.chess_national_id,
-            "black_player_score": match.black_player.score,
-            "player1_first_name": match.player1.first_name,
-            "player1_last_name": match.player1.last_name,
-            "player1_date_of_birth": match.player1.date_of_birth,
-            "player1_chess_national_id": match.player1.chess_national_id,
-            "player1_score": match.player1.score,
-            "player2_first_name": match.player2.first_name,
-            "player2_last_name": match.player2.last_name,
-            "player2_date_of_birth": match.player2.date_of_birth,
-            "player2_chess_national_id": match.player2.chess_national_id,
-            "player2_score": match.player2.score,
-            "result": match.result.name,
-            "round_id": round_id
-        }
-        match_table.insert(match_data)
-
-    def checkpoint_players(self, players_to_save: Union[List[Model.Player], Model.Tournament]):
-        players_table = self.data_base_temporary_tournament.table("players")
-        players_table.truncate()
-        if isinstance(players_to_save, List):
-            for player in players_to_save:
-                player_data = {
-                    "first_name": player.first_name,
-                    "last_name": player.last_name,
-                    "date_of_birth": player.date_of_birth,
-                    "chess_national_id": player.chess_national_id,
-                    "score": player.score,
-                    "tournament_id": self.tournament_temporary_id
-                }
-                players_table.insert(player_data)
-        if isinstance(players_to_save, Model.Tournament):
-            for player in players_to_save.players:
-                player_data = {
-                    "first_name": player.first_name,
-                    "last_name": player.last_name,
-                    "date_of_birth": player.date_of_birth,
-                    "chess_national_id": player.chess_national_id,
-                    "score": player.score,
-                    "tournament_id": self.tournament_temporary_id
-                }
-                players_table.insert(player_data)
-
-    def checkpoint_round(self, rounds_to_save: List[Model.Round]):
-        rounds_table = self.data_base_temporary_tournament.table("rounds")
-        matches_table = self.data_base_temporary_tournament.table("matches")
-        matches_table.truncate()
-        rounds_table.truncate()
-        for round in rounds_to_save:
-            round_data = {
-                "name": round.name,
-                "start_date": round.start_datetime,
-                "tournament_id": self.tournament_temporary_id
-            }
-            round_id = rounds_table.insert(round_data)
-            for match in round.list_of_matches:
-                self.checkpoint_match(match, round_id)
+            return None
 
     def delete_unfinished_tournament(self):
-        tournament_table = self.data_base_temporary_tournament.table("tournaments")
-        rounds_table = self.data_base_temporary_tournament.table("rounds")
-        matches_table = self.data_base_temporary_tournament.table("matches")
-        players_table = self.data_base_temporary_tournament.table("players")
+        """
+        Deletes the unfinished tournament data from the cache.
+        """
+        tournament_table = self.data_base_cache.table("tournaments")
         tournament_table.truncate()
-        rounds_table.truncate()
-        matches_table.truncate()
-        players_table.truncate()
-        self.data_base_temporary_tournament.close()
-
-    def load_unfinished_tournament(self):
-        tournament: Model.Tournament
-        tournament_data = self.data_base_temporary_tournament.table("tournaments").all()[0]
-        self.tournament_temporary_id = tournament_data.doc_id
-        name = tournament_data.get("name", None)
-        location = tournament_data.get("location", None)
-        start_date = tournament_data.get("start_date", None)
-        if all(item is not None for item in [name, location, start_date]):
-            tournament = Model.Tournament(name, location)
-            print(f"Unfinished tournament name = {tournament.name}")
-            print(f"Unfinished tournament location = {tournament.location}")
-            print(f"Unfinished tournament start date: = {tournament.start_date}")
-            self.load_unfinished_round(id_tournament=self.tournament_temporary_id)
-            
-    def load_unfinished_round(self, id_tournament: int) -> Model.Round:
-        rounds_table = self.data_base_temporary_tournament.table("rounds")
-        rounds = rounds_table.all()
-        for round in rounds:
-            round_id = round.doc_id
-            print(round_id)
-            self.load_unfinished_match(round_id)
-
-    def load_unfinished_match(self, id_round: int):
-        matches_table = self.data_base_temporary_tournament.table("matches")
-        matches = matches_table.search(Query().round_id == id_round)
-        for match in matches:
-            print(len(matches))
-            print(match["result"])
