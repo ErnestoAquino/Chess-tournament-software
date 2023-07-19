39b28fbc7235397ac1da1e2561e3351460a0dbd3
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-07-19 14:49:11 +0200

    The documentation for the methods of the class has been added.

--- Controller/AddPlayerController.py
+++ Controller/AddPlayerController.py
@@ -1,23 +1,41 @@
-from tinydb import TinyDB
-import View
+from typing import List
+from Model import Player
+from DataBaseManager import DataBaseManager
+from View import AddPlayerView
 
 
 class AddPlayerController:
-    DATA_BASE = TinyDB("Data/Players/Players.json")
-    players = []
+    """
+    The AddPlayerController class manages the process of adding and saving new players.
+    """
+    players: List[Player] = []
 
     def __init__(self):
-        self.add_player_view = View.AddPlayerView()
+        """
+        Initializes an instance of the AddPlayerController class.
+
+        The constructor creates an instance of the AddPlayerView to handle user input
+        and an instance of the DataBaseManager to manage database interactions.
+        """
+        self.add_player_view = AddPlayerView()
+        self.database_manager = DataBaseManager()
 
     def get_players_to_save(self):
+        """
+        Retrieves the players to be saved from the AddPlayerView.
+
+        This method uses the AddPlayerView to prompt the user for player information,
+        and then stores the entered players in the 'players' attribute.
+        """
         self.players = self.add_player_view.players_to_save()
         self.save_players()
 
     def save_players(self):
-        local_players = self.DATA_BASE.table("Players")
-        for player in self.players:
-            local_players.insert({"first_name": player.first_name,
-                                  "last_name": player.last_name,
-                                  "date_of_birth": player.date_of_birth,
-                                  "chess_national_id": player.chess_national_id,
-                                  "score": player.score})
+        """
+        Saves the list of players to the database using the DataBaseManager.
+
+        This method calls the 'save_players' method of the DataBaseManager,
+        passing the list of players to be saved. The database manager handles
+        the process of storing the player data in the database.
+        """
+        self.database_manager.save_players(self.players)
